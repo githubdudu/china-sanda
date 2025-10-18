17f72d5d28795824786ad9eb46d55de19edda7e3
dudu_win10 <hdon694@aucklanduni.ac.nz>
2025-10-18 13:32:24 +1300

    feat: update ESLint configuration to use eslint:recommendation

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,18 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended, // optional unless you're using "eslint:recommended" https://github.com/eslint/eslintrc#usage
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("eslint:recommended", "next/core-web-vitals", "next/typescript"),
   {
     ignores: [
       "node_modules/**",
